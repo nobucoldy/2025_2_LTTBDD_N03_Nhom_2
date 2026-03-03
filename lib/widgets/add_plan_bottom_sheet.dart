d24d4684d43839461ab2de4b188c4215a78c2a8a
bac <quebac2385@gmail.com>
2026-03-03 23:13:20 +0700

    update

--- lib/widgets/add_plan_bottom_sheet.dart
+++ lib/widgets/add_plan_bottom_sheet.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
-class AddPlanBottomSheet extends StatelessWidget {
+class AddPlanBottomSheet extends StatefulWidget {
   const AddPlanBottomSheet({super.key});
 
   @override
+  State<AddPlanBottomSheet> createState() => _AddPlanBottomSheetState();
+}
+
+class _AddPlanBottomSheetState extends State<AddPlanBottomSheet> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.9,
-      padding: const EdgeInsets.all(16),
-      child: const Center(child: Text('Thêm')),
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.5,
+      child: Column(
+        children: [
+          TextField(),
+          const SizedBox(height: 16),
+          Row(children: [Icon(Icons.add_circle_outline_outlined)]),
+        ],
+      ),
     );
   }
 }
